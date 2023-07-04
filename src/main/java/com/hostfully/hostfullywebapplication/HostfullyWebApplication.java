f00c67753d487dc59e8a9995b1c56226f2c3abee
Zach Ayers <z.ayers@icloud.com>
2023-07-04 14:52:41 -0600

    chore: optimize imports

--- src/main/java/com/hostfully/hostfullywebapplication/HostfullyWebApplication.java
+++ src/main/java/com/hostfully/hostfullywebapplication/HostfullyWebApplication.java
@@ -2,16 +2,6 @@ package com.hostfully.hostfullywebapplication;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Profile;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-import springfox.documentation.builders.PathSelectors;
-import springfox.documentation.builders.RequestHandlerSelectors;
-import springfox.documentation.spi.DocumentationType;
-import springfox.documentation.spring.web.plugins.Docket;
 
 /**
  * The HostfullyWebApplication class is the main entry point for the application.
